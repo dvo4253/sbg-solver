4de455f7d448c4e26d3e090ba515363f10088d77
Nathan Dvorachek <8908406+dvo4253@users.noreply.github.com>
2018-10-10 20:50:49 -0500

    Iterative Depth First Search complete

--- src/Search.py
+++ src/Search.py
@@ -51,11 +51,9 @@ def BFS(graph, startState):
         node = queue.dequeue()
         if (node.hash not in visited):
             i += 1
-            # print("Move #: " + str(i))
+
             visited.append(node.hash)
             inGoal = stateUtils.inGoalCheck(node.state)
-            
-            #stateUtils.printState(node.state)
 
             moves = stateUtils.getValidMoves(node.state)
             node.moves = moves
@@ -73,45 +71,35 @@ def BFS(graph, startState):
     return visited
     
 
-def DFS(graph, startState, inGoal):
-    queue = Queue()
+def DFS(graph, startState, inGoal, iterative):
 
+    if(iterative):
+        MAX_DEPTH = 1
+    else:
+        MAX_DEPTH = -1
+
+    current_depth = 0
     currentNode = Node(startState)
     
     graph[currentNode.hash] = [currentNode]
-    # queue.enqueue(currentNode)
-    visited = []
+ 
+    while (not inGoal):
+        visited = []
+        inGoal = DFSHelper(graph, startState, inGoal, visited, MAX_DEPTH, current_depth)
+        MAX_DEPTH += 1
 
-    # i = 0
-
-    # moves = stateUtils.getValidMoves(startState)
-    # for move in moves:
-    #     print(move)
-    
-    # for hash in visited:
-    #     print("Hash: " + hash)
-
-    DFSHelper(graph, startState, inGoal, visited)
-
-    # print("FINAL STATE: ")
-    # print(visited[-1])
-    # stateUtils.printState(graph[visited[-1]]c[0].parent[0].state)
     return visited
 
 
-def DFSHelper(graph, state, inGoal, visited):
+def DFSHelper(graph, state, inGoal, visited, MAX_DEPTH, current_depth):
 
     currentNode = Node(state)
     moves = stateUtils.getValidMoves(state)
-    
-    # c
-
 
     while moves and not inGoal:
         move = moves.pop()
         childState = stateUtils.makeMove(state, move)
-        # cinput("Press Enter to continue...")
-        # stateUtils.printState(childState)
+       
         childNode = Node(childState)
 
         inGoal = stateUtils.inGoalCheck(childState)
@@ -126,44 +114,69 @@ def DFSHelper(graph, state, inGoal, visited):
         if (childNode.hash not in graph.keys()):
             graph[childNode.hash] = [childNode]
 
-
-        # if (inGoal):
-            # print("Goal Hash:")
-            # print(childNode.hash)
-            # stateUtils.printState(childState)
-            # PrintSearchResults(graph, childNode.hash, 5)
-
-
-        if (childNode.hash not in visited and not inGoal):
+        if (childNode.hash not in visited and not inGoal and (MAX_DEPTH < 0 or current_depth < MAX_DEPTH)):
             visited.append(childNode.hash)
-            inGoal = DFSHelper(graph, childState, inGoal, visited)
+            inGoal = DFSHelper(graph, childState, inGoal, visited, MAX_DEPTH, current_depth + 1)
         elif (inGoal):
             visited.append(childNode.hash)
 
-    # print(visited)
     return inGoal
 
-def PrintSearchResults(graph, goalStateHash, totalSecs):
-    # Item 0 is the node itself
-    parent = graph[goalStateHash][0].parent
-    finalPath = []
-    # Navigate through parent nodes to
-    # retrieve the path to the goal node.
-    while parent: 
-        finalPath.append(parent)
-        # print(parent[1])
-        if hasattr(parent[0],'parent'):
-            parent = parent[0].parent
+# def PrintSearchResults(graph, goalStateHash, totalSecs):
+#     # Item 0 is the node itself
+#     parent = graph[goalStateHash][0].parent
+#     finalPath = []
+#     # Navigate through parent nodes to
+#     # retrieve the path to the goal node.
+#     while parent: 
+#         finalPath.append(parent)
+#         # print(parent[1])
+#         if hasattr(parent[0],'parent'):
+#             parent = parent[0].parent
+#         else:
+#             parent = None
+
+#     # move = finalPath.pop()
+#     pathLength = len(finalPath)
+#     while finalPath: 
+#         result = finalPath.pop()
+#         # stateUtils.printState(result[0].state)
+#         print(result[1])
+
+#     stateUtils.printState(graph[goalStateHash][0].state)
+
+#     print(str(len(graph)) + " " + str(round(totalSecs, 5)) + " " + str(pathLength))
+
+
+
+
+def PrintSearchResults(label, dictionary, visited, totalSecs):
+    goalNode = dictionary[visited[-1]][0]
+    node = goalNode
+    parentHash = node.parent[0].hash
+    parentMove = node.parent[1]
+    # print("PARENT MOVE: ")
+    # print(parentMove)
+    
+    print()
+    print(label)
+
+    # stateUtils.printState(state)
+    finalPath = [ parentMove ]
+    while parentHash != None:
+        node = dictionary[parentHash][0]
+        
+        # stateUtils.printState(node.state)
+        if hasattr(node,'parent'):
+            finalPath.append(node.parent[1])
+            parentHash = node.parent[0].hash
+            # stateUtils.printState(parent[0].state)
         else:
-            parent = None
+            parentHash = None
+        
+    for move in reversed(finalPath):
+        print(move)
+    
+    stateUtils.printState(goalNode.state)
 
-    # move = finalPath.pop()
-    pathLength = len(finalPath)
-    while finalPath: 
-        result = finalPath.pop()
-        # stateUtils.printState(result[0].state)
-        print(result[1])
-
-    stateUtils.printState(graph[goalStateHash][0].state)
-
-    print(str(len(graph)) + " " + str(round(totalSecs, 5)) + " " + str(pathLength))
+    print(str(len(dictionary)) + " " + str(round(totalSecs, 5)) + " " + str(len(finalPath)))
--- src/main.py
+++ src/main.py
@@ -1,29 +1,16 @@
-from src.Node import Node
 import time
 import sys
-# import queue
 import src.stateUtils as stateUtils
-from src.Node import Node
 import src.Search
 
 def execute(fileName):
     
     # # of steps taken in Random Walk
     N = 3
-
     # Get the initial state file from input
-    #inputStateFile = sys.argv[1]
     inputStateFile = fileName
     # Read in the state file to a matrix
     state = stateUtils.readGameState(inputStateFile)
-
-    # state = stateUtils.normalizeState(state)
-   
-    # stateUtils.printState(state)
-    # currentNode = Node(state)
-
-    # print("Random Walk (N=3)")
-    # src.Search.Random(state, N)
 
     dictionary = {}
     start = time.time()
@@ -31,82 +18,23 @@ def execute(fileName):
     end = time.time()
     totalSecs = end - start
     
-    print()
-    print("Breadth-First Search")
-    src.Search.PrintSearchResults(dictionary, visited[-1], totalSecs)
+    src.Search.PrintSearchResults("Breadth-First Search", dictionary, visited, totalSecs)
 
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     dictionary = {}
     start = time.time()
-    visited = src.Search.DFS(dictionary, state, False)
+    visited = src.Search.DFS(dictionary, state, False, False)
     end = time.time()
     totalSecs = end - start
-    # print("VISITED: ")
-    # print(visited[-1])
-    # for hash in visited:
-    #     print(str(hash))
-    # print(dictionary[visited[-1]])
-    # for node in dictionary[visited[-1]]:
-    #     parentHash = print(node.parent[0].hash)
-    #     stateUtils.printState(node.state)
-
-    # print("DICTIONARY: ")
-    goalNode = dictionary[visited[-1]][0]
-    node = goalNode
-    parentHash = node.parent[0].hash
-    parentMove = node.parent[1]
-    # print("PARENT MOVE: ")
-    # print(parentMove)
     
-    print()
-    print("Depth-First Search")
-
-    stateUtils.printState(state)
-    finalPath = [ parentMove ]
-    while parentHash != None:
-        node = dictionary[parentHash][0]
-        
-        # stateUtils.printState(node.state)
-        if hasattr(node,'parent'):
-            finalPath.append(node.parent[1])
-            parentHash = node.parent[0].hash
-            # stateUtils.printState(parent[0].state)
-        else:
-            parentHash = None
-        
-    for move in reversed(finalPath):
-        print(move)
-    
-    stateUtils.printState(goalNode.state)
-
-    print(str(len(dictionary)) + " " + str(round(totalSecs, 5)) + " " + str(len(finalPath)))
-    # for hash in dictionary:
-    #     print(str(hash))
-        
-    #     if(hasattr(dictionary[hash][0], "parent")):
-    #         print(str(dictionary[hash][0].parent[0].hash))
-    #         stateUtils.printState(dictionary[hash][0].parent[0].state)
-    #     else:
-    #         print("NO PARENT: ")
-
-    # parent = dictionary[visited[-1]][0].parent[0]
-    # finalPath = []
-    # # Navigate through parent nodes to
-    # # retrieve the path to the goal node.
-    # while parent: 
-    #     finalPath.append(parent)
-    #     # print(parent[1])
-    #     if hasattr(parent[0],'parent'):
-    #         parent = dictionary[parent[0].hash][0]
-    #         stateUtils.printState(parent[0].state)
-    #     else:
-    #         parent = None
-
+    src.Search.PrintSearchResults("Depth-First Search", dictionary, visited, totalSecs)
 
         
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    dictionary = {}
+    start = time.time()
+    visited = src.Search.DFS(dictionary, state, False, True)
+    end = time.time()
+    totalSecs = end - start
 
-
-
-
-    # src.Search.PrintSearchResults(dictionary, visited[-1], totalSecs)
-   
+    src.Search.PrintSearchResults("Iterative Depth-First Search", dictionary, visited, totalSecs)
